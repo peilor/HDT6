206a3b3a7217e9a34fd6e5ff438beee7a560fa81
peilor <arg13028@uvg.edu.gt>
2014-08-29 08:25:22 -0600

    Cambio 0.0.2

--- HDT6/src/Main.java
+++ HDT6/src/Main.java
@@ -4,8 +4,48 @@
  * @author Héctor Möller, Carnet: 09002
  * 
  */
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 public class Main {
+    private static int tipo;
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean cont=true;
+        while(cont){
+        int op=Integer.parseInt(JOptionPane.showInputDialog("¿Que tipo de implementación de Set desea?"
+                + "\n1)HashSet"
+                + "\n2)TreeSet"
+                + "\n3)LinkedHashSet"));
+        if(op==1){
+            tipo=1;
+            cont=false;
+        }else if(op==2){
+            tipo=2;
+            cont=false;
+        }else if(op==3){
+            tipo=3;
+            cont=false;
+        }else{
+            JOptionPane.showMessageDialog(null, "Usted no ha ingresado una opcion valida");
+            }
+        }
+        String persona=JOptionPane.showInputDialog("¿Cual es su nombre?");
+        String[] opciones = {"Si", "No"};
+        
+        while (true) {
+            int respuesta = JOptionPane.showOptionDialog(
+                               null
+                             , "¿Es usted un desarrollador de Java?"
+                             , persona
+                             , JOptionPane.YES_NO_OPTION
+                             , JOptionPane.PLAIN_MESSAGE
+                             , null
+                             , opciones
+                             , "Si");
+        if(respuesta==1){
+            
+        }
     }
+}
 }
