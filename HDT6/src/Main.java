7dc9d07f4d3d21bdf23391a796ce2dea0cf78fa2
peilor <arg13028@uvg.edu.gt>
2014-08-29 07:58:35 -0600

    Cambio 0.0.1

---
+++ HDT6/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Pablo Argueta, Carnet: 13028
+ * @author Héctor Möller, Carnet: 09002
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
