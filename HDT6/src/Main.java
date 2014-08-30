6626dcb38ab43b0a6691f7c4e15df4bfdd8bba4a
peilor <arg13028@uvg.edu.gt>
2014-08-29 18:52:21 -0600

    Cambio 0.0.3

--- HDT6/src/Main.java
+++ HDT6/src/Main.java
@@ -4,48 +4,91 @@
  * @author Héctor Möller, Carnet: 09002
  * 
  */
-import java.util.Collections;
-import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Main {
     private static int tipo;
+    private static boolean desJava;
+    private static boolean desWeb;
+    private static boolean desCel;
     public static void main(String[] args) {
         boolean cont=true;
         while(cont){
-        int op=Integer.parseInt(JOptionPane.showInputDialog("¿Que tipo de implementación de Set desea?"
-                + "\n1)HashSet"
-                + "\n2)TreeSet"
-                + "\n3)LinkedHashSet"));
-        if(op==1){
-            tipo=1;
-            cont=false;
-        }else if(op==2){
-            tipo=2;
-            cont=false;
-        }else if(op==3){
-            tipo=3;
-            cont=false;
-        }else{
-            JOptionPane.showMessageDialog(null, "Usted no ha ingresado una opcion valida");
-            }
+            int op=Integer.parseInt(JOptionPane.showInputDialog("¿Que tipo de implementación de Set desea?"
+                    + "\n1)HashSet"
+                    + "\n2)TreeSet"
+                    + "\n3)LinkedHashSet"));
+            if(op==1){
+                tipo=1;
+                cont=false;
+            }else if(op==2){
+                tipo=2;
+                cont=false;
+            }else if(op==3){
+                tipo=3;
+                cont=false;
+            }else{
+                JOptionPane.showMessageDialog(null, "Usted no ha ingresado una opcion valida");
+                }
         }
-        String persona=JOptionPane.showInputDialog("¿Cual es su nombre?");
+        SetFactory sFactory = new SetFactory();
+        Set datos = sFactory.getSet(tipo);
         String[] opciones = {"Si", "No"};
-        
-        while (true) {
+        boolean bandera=true;        
+        while (bandera) {
+            desWeb=false;
+            desWeb=false;
+            desWeb=false;
+            String persona=JOptionPane.showInputDialog("¿Cual es su nombre?");
             int respuesta = JOptionPane.showOptionDialog(
-                               null
-                             , "¿Es usted un desarrollador de Java?"
-                             , persona
-                             , JOptionPane.YES_NO_OPTION
-                             , JOptionPane.PLAIN_MESSAGE
-                             , null
-                             , opciones
-                             , "Si");
-        if(respuesta==1){
-            
-        }
-    }
+                    null
+                    , "¿Es usted un desarrollador de Java?"
+                    , persona
+                    , JOptionPane.YES_NO_OPTION
+                    , JOptionPane.PLAIN_MESSAGE
+                    , null
+                    , opciones
+                    , "Si");
+            if(respuesta==0){
+                desJava=true;
+            }
+            respuesta = JOptionPane.showOptionDialog(
+                    null
+                    , "¿Es usted un desarrollador Web?"
+                    , persona
+                    , JOptionPane.YES_NO_OPTION
+                    , JOptionPane.PLAIN_MESSAGE
+                    , null
+                    , opciones
+                    , "Si");
+            if(respuesta==0){
+                desWeb=true;
+            }
+            respuesta = JOptionPane.showOptionDialog(
+                    null
+                    , "¿Es usted un desarrollador de Celular?"
+                    , persona
+                    , JOptionPane.YES_NO_OPTION
+                    , JOptionPane.PLAIN_MESSAGE
+                    , null
+                    , opciones
+                    , "Si");
+            if(respuesta==0){
+                desCel=true;
+            }
+            respuesta = JOptionPane.showOptionDialog(
+                    null
+                    , "¿Desea ingresar otro usuario?"
+                    , persona
+                    , JOptionPane.YES_NO_OPTION
+                    , JOptionPane.PLAIN_MESSAGE
+                    , null
+                    , opciones
+                    , "Si");
+            Persona newPersona = new Persona(persona,desJava,desWeb,desCel);
+            if(respuesta==1){
+                bandera=false;
+            }
+    } 
 }
 }
